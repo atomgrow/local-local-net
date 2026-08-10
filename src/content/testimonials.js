2b188884d3b5434cbf3b9e3504f7d79e66d7ea00
Tomas Vukojicic <tomas@topcret.com>
2026-08-10 06:42:47 -0300

    Fix testimonios encoding and content

--- src/content/testimonials.js
+++ src/content/testimonials.js
@@ -16,7 +16,7 @@ export const TESTIMONIALS = {
     {
       isDemo: false,
       texto:
-        'Estábamos bastante decididos, pero teníamos muchas dudas sueltas. Bernardo nos ayudó a ponerlas sobre la mesa y nos conectó con gente que nos dio mucha confianza.',
+        'Estabamos bastante decididos, pero teniamos muchas dudas sueltas. Bernardo nos ayudo a ponerlas sobre la mesa y nos conecto con gente que nos dio mucha confianza.',
       nombre: 'Rodrigo Zubillaga y flia.',
       relacion: '',
       ubicacion: '',
@@ -25,8 +25,8 @@ export const TESTIMONIALS = {
     {
       isDemo: false,
       texto:
-        'Llegué a Bernardo con una consulta bastante puntual y, hablando, aparecieron varias cosas que no había tenido en cuenta. Me ayudó a ordenar todo y a decidir con más tranquilidad.',
-      nombre: 'Mónica Guastella',
+        'Llegue a Bernardo con una consulta bastante puntual y, hablando, aparecieron varias cosas que no habia tenido en cuenta. Me ayudo a ordenar todo y a decidir con mas tranquilidad.',
+      nombre: 'Monica Guastella',
       relacion: 'inversora',
       ubicacion: '',
       foto: null,
@@ -34,8 +34,8 @@ export const TESTIMONIALS = {
     {
       isDemo: false,
       texto:
-        'Bernardo y equipo conocen muy bien Tandil, pero, sobre todo, saben quién es quién y cómo trabaja cada uno. Eso fue lo que más confianza nos dio.',
-      nombre: 'Andrea Arévalo y flia.',
+        'Bernardo y equipo conocen muy bien Tandil, pero, sobre todo, saben quien es quien y como trabaja cada uno. Eso fue lo que mas confianza nos dio.',
+      nombre: 'Andrea Arevalo y flia.',
       relacion: '',
       ubicacion: '',
       foto: null,
@@ -43,7 +43,7 @@ export const TESTIMONIALS = {
     {
       isDemo: false,
       texto:
-        'Estábamos por poner plata en un proyecto que parecía una buena oportunidad. Bernardo empezó a hacer preguntas que nadie había hecho y aparecieron varias cosas que no cerraban. Frenamos a tiempo. Viéndolo ahora, nos evitó un problema enorme.',
+        'Estabamos por poner plata en un proyecto que parecia una buena oportunidad. Bernardo empezo a hacer preguntas que nadie habia hecho y aparecieron varias cosas que no cerraban. Frenamos a tiempo. Viendolo ahora, nos evito un problema enorme.',
       nombre: 'Francisco Bravo',
       relacion: 'inversor CABA',
       ubicacion: '',
@@ -54,37 +54,37 @@ export const TESTIMONIALS = {
     {
       isDemo: false,
       texto:
-        ‘We were pretty decided, but we had many loose threads. Bernardo helped us put them on the table and connected us with people who gave us a lot of confidence.’,
-      nombre: ‘Rodrigo Zubillaga and family’,
-      relacion: ‘’,
-      ubicacion: ‘’,
+        'We were pretty decided, but we had many loose threads. Bernardo helped us put them on the table and connected us with people who gave us a lot of confidence.',
+      nombre: 'Rodrigo Zubillaga and family',
+      relacion: '',
+      ubicacion: '',
       foto: null,
     },
     {
       isDemo: false,
       texto:
-        ‘I came to Bernardo with a fairly specific question, and as we talked, several things came up that I had not considered. He helped me organize everything and make the decision with more peace of mind.’,
-      nombre: ‘Mónica Guastella’,
-      relacion: ‘investor’,
-      ubicacion: ‘’,
+        'I came to Bernardo with a fairly specific question, and as we talked, several things came up that I had not considered. He helped me organize everything and make the decision with more peace of mind.',
+      nombre: 'Monica Guastella',
+      relacion: 'investor',
+      ubicacion: '',
       foto: null,
     },
     {
       isDemo: false,
       texto:
-        ‘Bernardo and his team know Tandil very well, but above all, they know who is who and how each person works. That is what gave us the most confidence.’,
-      nombre: ‘Andrea Arévalo and family’,
-      relacion: ‘’,
-      ubicacion: ‘’,
+        'Bernardo and his team know Tandil very well, but above all, they know who is who and how each person works. That is what gave us the most confidence.',
+      nombre: 'Andrea Arevalo and family',
+      relacion: '',
+      ubicacion: '',
       foto: null,
     },
     {
       isDemo: false,
       texto:
-        ‘We were about to put money into a project that looked like a good opportunity. Bernardo began to ask questions that no one else had asked, and several things that did not add up appeared. We stopped in time. Looking back, he saved us from a huge problem.’,
-      nombre: ‘Francisco Bravo’,
-      relacion: ‘investor Buenos Aires’,
-      ubicacion: ‘’,
+        'We were about to put money into a project that looked like a good opportunity. Bernardo began to ask questions that no one else had asked, and several things that did not add up appeared. We stopped in time. Looking back, he saved us from a huge problem.',
+      nombre: 'Francisco Bravo',
+      relacion: 'investor Buenos Aires',
+      ubicacion: '',
       foto: null,
     },
   ],
